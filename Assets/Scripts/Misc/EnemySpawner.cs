06cc792f3392d174ac04d0affde1dc130b517702
unknown <joseph.demarest@cix.csi.cuny.edu>
2023-12-06 18:05:22 -0500

    update enemies and enemyspawner

--- Assets/Scripts/Misc/EnemySpawner.cs
+++ Assets/Scripts/Misc/EnemySpawner.cs
@@ -18,36 +18,18 @@ public class EnemySpawner : MonoBehaviour
     void SpawnRepeating()
     {
         elapsedTime += rate;
-        ScaleDifficulty();
         int enemyTypeIndex = GetEnemyTypeIndex();
         CreateEnemiesAroundPoint(1, PlayerData.instance.PlayerEntity.EntityObject.transform.position, 15f, enemyTypeIndex);
     }
 
-    // Scale the difficulty by adjusting the spawn rate
-    private void ScaleDifficulty()
-    {
-        // Adjust the rate more gradually
-        if (elapsedTime < 300) // Scale up to 5 minutes
-        {
-            float rateDecrease = (StartRate - 0.5f) * (elapsedTime / 300); // More gradual decrease
-            rate = StartRate - rateDecrease;
-            CancelInvoke("SpawnRepeating");
-            InvokeRepeating("SpawnRepeating", rate, rate);
-        }
-    }
-
-    // Determine which enemy type to spawn based on elapsed time
     private int GetEnemyTypeIndex()
     {
-        float timeFactor = elapsedTime / 300; // Scale factor based on time
-        for (int i = EnemyTypes.Length - 1; i >= 0; i--)
-        {
-            if (Random.Range(0f, 1f) < timeFactor * (i + 1) / EnemyTypes.Length)
-            {
-                return i;
-            }
-        }
-        return 0;
+        // Gradually introduce new enemy types based on elapsed time
+        int maxIndex = Mathf.FloorToInt(elapsedTime / 60); // New type every 60 seconds
+        maxIndex = Mathf.Clamp(maxIndex, 0, EnemyTypes.Length - 1);
+
+        // Randomly select from available types
+        return Random.Range(0, maxIndex + 1);
     }
 
     public void CreateEnemiesAroundPoint(int num, Vector3 point, float radius, int enemyTypeIndex)
@@ -55,17 +37,11 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 0; i < num; i++)
         {
             float radians = Random.Range(0f, 2f * Mathf.PI);
+            float vertical = Mathf.Sin(radians);
+            float horizontal = Mathf.Cos(radians);
+            Vector3 spawnDir = new Vector3(horizontal, 0, vertical);
+            Vector3 spawnPos = point + spawnDir * radius;
 
-			/* Get the vector direction */
-			float vertical = Mathf.Sin(radians);
-			float horizontal = Mathf.Cos(radians);
-
-			Vector3 spawnDir = new Vector3(horizontal, 0, vertical);
-
-			/* Get the spawn position */
-			Vector3 spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
-
-            // Use enemyTypeIndex to spawn different enemy types
             GameObject enemyToSpawn = EnemyTypes[enemyTypeIndex];
             GameObject enemy = ObjectPool.instance.PullObject(enemyToSpawn.name);
             if (enemy != null)
